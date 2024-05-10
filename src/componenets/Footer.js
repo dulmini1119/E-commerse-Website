a948cd9cf4339dd56cbfb1540924cbccc703b7dc
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-05-10 14:06:55 +0530

    commit

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import newsletter from '../images/newsletter.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <>
+    <footer className='py-4'>
+      <div className="container-xxl">
+        <div className='row align-items-center'>
+          <div className='col-5'>
+            <div className='footer-top-data d-flex gap-30 align-items-center'>
+              <img src={newsletter} alt='newsletter'/>
+              <h2 className='text-white mb-0'>Sign Up for Newsletter</h2>
+            </div>
+          </div>
+
+          <div className='col-7'>
+          <div className='input-group'>
+              <input
+              type='text'
+              className='form-control py-1'
+              placeholder='Your Email Address'
+              aria-label='Your Email Address'
+              aria-describedby='basic-addon2'/>
+              <span className='input-group-text p-2' id='basic-addon2'>
+                Subscribe
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <footer className='py-4'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-4'>
+            <h4 className='text-white nb-4'>Information</h4>
+            <div></div>
+          </div>
+
+          <div className='col-3'>
+          <h4 className='text-white nb-4'>Contact Us</h4>
+          <div></div>
+          </div>
+
+          <div className='col-3'>
+          <h4 className='text-white nb-4'>Accounts</h4>
+          <div></div>
+          </div>
+
+          <div className='col-2'>
+          <h4 className='text-white nb-4'>Quick Links</h4>
+          <div></div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <footer className='py-4'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <p className='text-center mb-0 text-white'>&copy; {new Date().getFullYear()}; powered by y&b </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </>
+  );
 }
 
 export default Footer
